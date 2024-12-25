bc5f751f2d916aad2106e86974aaf36c2468b69a
vijay <vkmakwana1122@gmail.com>
2024-12-25 11:17:24 +0530

    add: message and users - router, controller, model, database

---
+++ controller/messageController.js
@@ -0,0 +1,17 @@
+import { catchAsyncErrors } from '../middlewares/catchAsyncErrors.js';
+import { Message } from '../models/messageSchema.js'
+import ErrorHandler from '../middlewares/errorMiddleware.js'
+
+export const sendMessage = catchAsyncErrors(async (req, res, next) => {
+    const { firstName, lastName, email, phone, message } = req.body;
+
+    if (!firstName || !lastName || !email || !phone || !message) {
+
+        return next(new ErrorHandler('please fill all fields', 400));
+    }
+    await Message.create({ firstName, lastName, email, phone, message });
+    res.status(200).json({
+        success: true,
+        Message: 'Message sent successfully'
+    });
+})
