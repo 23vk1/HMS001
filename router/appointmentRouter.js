bd0e746325903e01d9020d12f97c2298c817c145
vijay <vkmakwana1122@gmail.com>
2024-12-28 13:10:45 +0530

    added : appointment method

--- router/appointmentRouter.js
+++ router/appointmentRouter.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { postAppointment } from '../controller/appointmentController.js';
+import { deleteAppointment, getAllAppointment, postAppointment, updateAppointmentStatus } from '../controller/appointmentController.js';
 import { isPatientAuthanticated, isAdminAuthanticated } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post('/post',isPatientAuthanticated, postAppointment)
-
-
+router.get('/getall',isAdminAuthanticated, getAllAppointment)
+router.put('/update/:id',isAdminAuthanticated, updateAppointmentStatus)
+router.delete('/delete/:id',isAdminAuthanticated, deleteAppointment)
 
 
 export default router;
