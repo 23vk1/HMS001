6504f4f077bfc3fe884d01b116703dd3b937201c
23vk1 <154504485+23vk1@users.noreply.github.com>
2025-03-16 16:51:01 +0530

    Update appointmentController.js

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -5,9 +5,9 @@ import { User } from '../models/userSchema.js';
 
 
 export const postAppointment = catchAsyncErrors(async (req, res, next) => {
-    const { firstName, lastName, email, phone, nic, dob, gender, appointment_data, department, doctor_firstName, doctor_lastName, hasVisited, address } = req.body;
+    const { firstName, lastName, email, phone, nic, dob, gender, appointment_date, department, doctor_firstName, doctor_lastName, hasVisited, address } = req.body;
 
-    if (!firstName || !lastName || !email || !phone || !nic || !dob || !gender || !appointment_data || !department || !doctor_firstName || !doctor_lastName || !address) {
+    if (!firstName || !lastName || !email || !phone || !nic || !dob || !gender || !appointment_date || !department || !doctor_firstName || !doctor_lastName || !address) {
         return next(new ErrorHandler('Please Fill All Fieelds', 400));
     }
 
@@ -29,7 +29,7 @@ export const postAppointment = catchAsyncErrors(async (req, res, next) => {
     const patientId = req.user._id;
 
     const appointment = await Appointment.create({
-        firstName, lastName, email, phone, nic, dob, gender, appointment_data, department, doctor: { firstName: doctor_firstName, lastName: doctor_lastName }, hasVisited, address, doctorId, patientId,
+        firstName, lastName, email, phone, nic, dob, gender, appointment_date, department, doctor: { firstName: doctor_firstName, lastName: doctor_lastName }, hasVisited, address, doctorId, patientId,
     });
     res.status(200).json({
         success: true,
